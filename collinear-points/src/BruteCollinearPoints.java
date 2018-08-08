63461470d6c78889ed4c5a6a1e012dd30cac946a
georgiv <gd.vassilev@gmail.com>
2018-08-08 17:29:24 +0300

    Progress...

--- collinear-points/src/BruteCollinearPoints.java
+++ collinear-points/src/BruteCollinearPoints.java
@@ -1,15 +1,51 @@
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
+  private Point[] points;
+  private LineSegment[] ss;
+
   public BruteCollinearPoints(Point[] points) {
-    // finds all line segments containing 4 points
+    this.points = validate(points);
   }
 
   public int numberOfSegments() {
-    // the number of line segments
-    return 0;
+    if (ss == null) {
+      ss = findSegments();
+    }
+    return ss.length;
   }
 
   public LineSegment[] segments() {
-    // the line segments
+    if (ss == null) {
+      ss = findSegments();
+    }
+    return ss;
+  }
+
+  private Point[] validate(Point[] points) {
+    if (points == null) {
+      throw new IllegalArgumentException();
+    }
+    for (int i = 0; i < points.length; i++) {
+      if (points[i] == null) {
+        throw new IllegalArgumentException();
+      }
+    }
+    Arrays.sort(points);
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i].compareTo(points[i + 1]) == 0) {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    Point[] res = new Point[points.length];
+    for (int i = 0; i < points.length; i++) {
+      res[i] = points[i];
+    }
+    return res;
+  }
+
+  private LineSegment[] findSegments() {
     return null;
   }
 }
