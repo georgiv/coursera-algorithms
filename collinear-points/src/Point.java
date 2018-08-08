d5824eb3270ab84e96823d5422f3b98d8670b65e
georgiv <gd.vassilev@gmail.com>
2018-08-08 16:25:06 +0300

    Point class completed

--- collinear-points/src/Point.java
+++ collinear-points/src/Point.java
@@ -17,6 +17,14 @@ public class Point implements Comparable<Point> {
   private final int x; // x-coordinate of this point
   private final int y; // y-coordinate of this point
 
+  private class SlopeOrderComparator implements Comparator<Point> {
+    public int compare(Point a, Point b) {
+      Double slopeA = slopeTo(a);
+      Double slopeB = slopeTo(b);
+      return slopeA.compareTo(slopeB);
+    }
+  }
+
   /**
    * Initializes a new point.
    *
@@ -60,8 +68,14 @@ public class Point implements Comparable<Point> {
    * @return the slope between this point and the specified point
    */
   public double slopeTo(Point that) {
-    /* YOUR CODE HERE */
-    return 0.0;
+    if (compareTo(that) == 0) {
+      return Double.NEGATIVE_INFINITY;
+    } else if (x == that.x) {
+      return Double.POSITIVE_INFINITY;
+    } else if (y == that.y) {
+      return 0.0;
+    }
+    return (double) (that.y - y) / (that.x - x);
   }
 
   /**
@@ -77,7 +91,17 @@ public class Point implements Comparable<Point> {
    *         argument point
    */
   public int compareTo(Point that) {
-    /* YOUR CODE HERE */
+    if (y < that.y) {
+      return -1;
+    } else if (y > that.y) {
+      return 1;
+    } else {
+      if (x < that.x) {
+        return -1;
+      } else if (x > that.x) {
+        return 1;
+      }
+    }
     return 0;
   }
 
@@ -88,8 +112,7 @@ public class Point implements Comparable<Point> {
    * @return the Comparator that defines this ordering on points
    */
   public Comparator<Point> slopeOrder() {
-    /* YOUR CODE HERE */
-    return null;
+    return new SlopeOrderComparator();
   }
 
   /**
