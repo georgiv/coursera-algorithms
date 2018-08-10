1a6422e8d116396f18c50c9f5a9cb655e102912e
georgiv <gd.vassilev@gmail.com>
2018-08-10 19:00:43 +0300

    Working on faster algorithm...

--- collinear-points/src/FastCollinearPoints.java
+++ collinear-points/src/FastCollinearPoints.java
@@ -1,15 +1,56 @@
+import java.util.Arrays;
+
 public class FastCollinearPoints {
-  public FastCollinearPoints(Point[] points) {
-    // finds all line segments containing 4 or more points
-  }
+  private Point[] points;
+  private LineSegment[] ss;
+
+  public FastCollinearPoints(Point[] points) { this.points = validate(points); }
 
   public int numberOfSegments() {
-    // the number of line segments
-    return 0;
+    if (ss == null) {
+      ss = findSegments();
+    }
+    return ss.length;
   }
 
   public LineSegment[] segments() {
-    // the line segments
+    if (ss == null) {
+      ss = findSegments();
+    }
+
+    LineSegment[] res = new LineSegment[ss.length];
+    for (int i = 0; i < ss.length; i++) {
+      res[i] = ss[i];
+    }
+    return res;
+  }
+
+  private Point[] validate(Point[] points) {
+    if (points == null) {
+      throw new IllegalArgumentException();
+    }
+    for (int i = 0; i < points.length; i++) {
+      if (points[i] == null) {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    Point[] res = new Point[points.length];
+    for (int i = 0; i < points.length; i++) {
+      res[i] = points[i];
+    }
+
+    Arrays.sort(res);
+    for (int i = 0; i < res.length - 1; i++) {
+      if (res[i].compareTo(res[i + 1]) == 0) {
+        throw new IllegalArgumentException();
+      }
+    }
+
+    return res;
+  }
+
+  private LineSegment[] findSegments() {
     return null;
   }
 }
