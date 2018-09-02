7e16fe8fd058b551dde30e9e3ef22d4bea76079f
georgiv <gd.vassilev@gmail.com>
2018-09-02 23:51:27 +0300

    Working on timing...

--- collinear-points/src/FastCollinearPoints.java
+++ collinear-points/src/FastCollinearPoints.java
@@ -1,7 +1,4 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdOut;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FastCollinearPoints {
@@ -56,113 +53,42 @@ public class FastCollinearPoints {
 
   private LineSegment[] findSegments() {
     LineSegment[] res = new LineSegment[0];
+
     if (points.length < 4) {
       return res;
     }
 
-    Point[] lowers = new Point[0];
-    Point[] uppers = new Point[0];
+    Point[] cp = new Point[points.length];
+    for (int i = 0; i < points.length; i++) {
+      cp[i] = points[i];
+    }
 
     for (int i = 0; i < points.length; i++) {
-      Point[] others = new Point[points.length - 1];
-      double[] slopes = new double[points.length - 1];
-      for (int j = 0; j < points.length; j++) {
-        if (i == j) {
-          continue;
-        }
-        if (j < i) {
-          others[j] = points[j];
-          slopes[j] = points[i].slopeTo(points[j]);
-        } else {
-          others[j - 1] = points[j];
-          slopes[j - 1] = points[i].slopeTo(points[j]);
-        }
-      }
+      Arrays.sort(cp);
+      Arrays.sort(cp, points[i].slopeOrder());
 
-      Arrays.sort(others, points[i].slopeOrder());
-      Arrays.sort(slopes);
+      int start = 1;
+      int end = 1;
 
-      int maxInt = 1;
-      int maxIni = 0;
-      int currentInt = 1;
-      int currentIni = 0;
-      for (int j = 1; j < others.length; j++) {
-        if (slopes[j] == slopes[currentIni]) {
-          currentInt++;
-        } else {
-          currentInt = 1;
-          currentIni = j;
-          if (maxInt >= 3) {
-            Point lower = points[i].compareTo(others[maxIni]) < 0 ? points[i] : others[maxIni];
-            Point upper = points[i].compareTo(others[maxIni + maxInt - 1]) > 0 ? points[i] : others[maxIni + maxInt - 1];
+      while (start < cp.length) {
+        while (end < cp.length && points[i].slopeTo(cp[end]) == points[i].slopeTo(cp[start])) end++;
 
-            boolean existing = false;
-            for (int k = 0; k < lowers.length; k++) {
-              existing = lowers[k].compareTo(lower) == 0 && uppers[k].compareTo(upper) == 0;
-              if (existing) {
-                break;
-              }
+        if (end - start >= 3) {
+          Point startP = cp[start].compareTo(points[i]) < 0 ? cp[start] : points[i];
+          Point endP = cp[end - 1].compareTo(points[i]) > 0 ? cp[end - 1] : points[i];
+
+          if (points[i] == startP) {
+            LineSegment[] newRes = new LineSegment[res.length + 1];
+            for (int j = 0; j < res.length; j++) {
+              newRes[j] = res[j];
             }
-            if (!existing) {
-              Point[] newLowers = new Point[lowers.length + 1];
-              for (int k = 0; k < lowers.length; k++) {
-                newLowers[k] = lowers[k];
-              }
-              newLowers[newLowers.length - 1] = lower;
-              lowers = newLowers;
-
-              Point[] newUppers = new Point[uppers.length + 1];
-              for (int k = 0; k < uppers.length; k++) {
-                newUppers[k] = uppers[k];
-              }
-              newUppers[newUppers.length - 1] = upper;
-              uppers = newUppers;
-            }
-          }
-          maxInt = 1;
-          maxIni = j;
-        }
-
-        if (currentInt > maxInt) {
-          maxInt = currentInt;
-          maxIni = currentIni;
-        }
-      }
-
-      if (maxInt >= 3) {
-        Point lower = points[i].compareTo(others[maxIni]) < 0 ? points[i] : others[maxIni];
-        Point upper = points[i].compareTo(others[maxIni + maxInt - 1]) > 0 ? points[i] : others[maxIni + maxInt - 1];
-
-        boolean existing = false;
-        for (int j = 0; j < lowers.length; j++) {
-          existing = lowers[j].compareTo(lower) == 0 && uppers[j].compareTo(upper) == 0;
-          if (existing) {
-            break;
+            newRes[newRes.length - 1] = new LineSegment(startP, endP);
+            res = newRes;
           }
         }
-        if (!existing) {
-          Point[] newLowers = new Point[lowers.length + 1];
-          for (int j = 0; j < lowers.length; j++) {
-            newLowers[j] = lowers[j];
-          }
-          newLowers[newLowers.length - 1] = lower;
-          lowers = newLowers;
-
-          Point[] newUppers = new Point[uppers.length + 1];
-          for (int j = 0; j < uppers.length; j++) {
-            newUppers[j] = uppers[j];
-          }
-          newUppers[newUppers.length - 1] = upper;
-          uppers = newUppers;
-        }
+        start = end;
       }
     }
-
-    LineSegment[] newRes = new LineSegment[lowers.length];
-    for (int i = 0; i < newRes.length; i++) {
-      newRes[i] = new LineSegment(lowers[i], uppers[i]);
-    }
-    res = newRes;
 
     return res;
   }
